00c52b951f0a0c2266023dca7a55f99038284f07
maxx-css <hellomauriciorivera@gmail.com>
2025-04-12 12:24:20 -0700

    Missed one

--- sanity/env.js
+++ sanity/env.js
@@ -1,5 +1,5 @@
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-03-28'
+  process.env.local.NEXT_PUBLIC_SANITY_API_VERSION || '2025-03-28'
 
 export const dataset = process.env.local.NEXT_PUBLIC_SANITY_DATASET;
 export const projectId = process.env.local.NEXT_PUBLIC_SANITY_PROJECT_ID;
