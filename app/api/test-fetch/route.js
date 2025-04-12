cdc1db0c9a227e87d66ad5f05ce7b99d6edbecd9
maxx-css <hellomauriciorivera@gmail.com>
2025-04-12 13:29:07 -0700

    fixed localhost

--- app/api/test-fetch/route.js
+++ app/api/test-fetch/route.js
@@ -6,8 +6,8 @@ import { createClient } from '@sanity/client';
 const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   apiVersion: '2023-03-01',
-  dataset: 'production',
-  useCdn: false, // Set to false for fresh data
+  dataset: 'lukedataset',
+  useCdn: true, // Set to false for fresh data
 });
 
 export async function GET() {
--- app/page.js
+++ app/page.js
@@ -6,9 +6,9 @@ import FeaturedPortfolio from './components/FeaturedPortfolio';
 // Initialize the Sanity client (server-side)
 const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset:  'production',
+  dataset:  'lukedataset',
   apiVersion: '2023-03-01',
-  useCdn: false, // Setting to false for fresh data on server
+  useCdn: true, // Setting to false for fresh data on server
 });
 console.log('client', client)
 
